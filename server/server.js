5ca95cd8599805dc536c513a2ed211d97fbee40f
Rafeeq <cerafeeq@gmail.com>
2019-03-02 19:36:57 +0530

    Added POST /users

--- server/server.js
+++ server/server.js
@@ -96,6 +96,23 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User({
+    email: body.email,
+    password: body.password
+  });
+
+  user.save().then((doc) => {
+    return doc.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
