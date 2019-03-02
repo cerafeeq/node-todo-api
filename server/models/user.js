77428be0f636a5a5e75f9e9076719138df7217c0
Rafeeq <cerafeeq@gmail.com>
2019-03-02 20:52:21 +0530

    Added GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -50,6 +50,24 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+UserSchema.statics.findByToken = function(token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc1234');
+  } catch (e) {
+    // return new Promise((resolve, reject) => {
+    //   reject();
+    // });
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    _id: decoded._id
+  });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {
