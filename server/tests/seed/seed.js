b28c2441636fc5ea8e22468ac936d0327e214a51
Rafeeq <cerafeeq@gmail.com>
2019-03-04 00:02:53 +0530

    Added seed

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,54 @@
+const { ObjectID } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { Todo } = require('../../models/todo');
+
+const { User } = require('../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const users = [{
+  _id: userOneId,
+  email: 'rafeeqce@gmail.com',
+  password: 'password',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc1234').toString()
+  }]
+}, {
+    _id: userTwoId,
+    email: 'cerafeeq@gmail.com',
+    password: 'password'
+}];
+
+const todos = [{
+  _id: new ObjectID(),
+  text: 'First test todo'
+}, {
+  _id: new ObjectID(),
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
+}];
+
+const populateTodos = (done) => {
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
+};
+
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
+
+    return Promise.all([userOne, userTwo])
+  }).then(() => done());
+};
+
+module.exports = {
+  todos,
+  populateTodos,
+  users,
+  populateUsers
+};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,24 +3,12 @@ const request = require('supertest');
 
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
+const { todos, populateTodos, users, populateUsers } = require('./seed/seed');
 
 const {ObjectID} = require('mongodb');
 
-const todos = [{
-  _id: new ObjectID(),
-  text: 'First test todo'
-}, {
-  _id: new ObjectID(),
-  text: 'Second test todo',
-  completed: true,
-  completedAt: 333
-}];
-
-beforeEach((done) => {
-  Todo.remove({}).then(() => {
-    return Todo.insertMany(todos);
-  }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
