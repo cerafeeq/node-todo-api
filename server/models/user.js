d8759d98c594706b5301c03b8e955f09c3d956dd
Rafeeq <cerafeeq@gmail.com>
2019-03-17 21:32:12 +0400

    Added test method for user logout

--- server/models/user.js
+++ server/models/user.js
@@ -56,9 +56,7 @@ UserSchema.methods.removeToken = function(token) {
 
   return user.update({
     $pull: {
-      tokens: {
-        token: token
-      }
+      tokens: { token }
     }
   });
 };
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -284,3 +284,26 @@ describe('POST /users/login', () => {
 
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    // DELETE /users/met/token
+    // SET x-auth equal to token
+    // 200
+    // Find user, verify that the tokens array has zero length
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
